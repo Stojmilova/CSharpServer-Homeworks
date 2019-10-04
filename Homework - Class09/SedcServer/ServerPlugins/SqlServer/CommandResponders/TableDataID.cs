2cbbb0903bba2728f040e727bcab9924b50f207a
Cvetanka <45522819+Stojmilova@users.noreply.github.com>
2019-10-05 00:51:13 +0200

    initial commit

---
+++ Homework - Class09/SedcServer/ServerPlugins/SqlServer/CommandResponders/TableDataID.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServerInterfaces;
+
+namespace ServerPlugins.SqlServer.CommandResponders
+{
+    public class TableDataID : ICommandResponder
+    {
+        public string ConnectionString { get; private set; }
+        public string TableName { get; private set; }
+        public int ID { get; private set; }
+
+        public TableDataID(string connectionString, string tableName, int Id)
+        {
+            ConnectionString = connectionString;
+            TableName = tableName;
+            ID = Id;
+        }
+        public async Task<Response> GetResponse()
+        {
+            using (var cnn = new SqlConnection(ConnectionString))
+            {
+                cnn.Open();
+                using (var command = new SqlCommand($@"SELECT * FROM {TableName} WHERE ID = {ID}", cnn))
+                {
+                    using (var dr = await command.ExecuteReaderAsync())
+                    {
+                        var schema = dr.GetColumnSchema();
+                        var columnNames = schema.Select(cs => cs.ColumnName);
+                        object[] values = new object[columnNames.Count()];
+
+                        var results = new List<Dictionary<string, string>>();
+
+                        while (dr.Read())
+                        {
+                            var columnCount = dr.GetValues(values);
+                            var objectDictionary = new Dictionary<string, string>();
+                            foreach (var column in schema)
+                            {
+
+                                objectDictionary.Add(column.ColumnName, dr.GetValue(column.ColumnOrdinal.Value).ToString());
+                            }
+                            results.Add(objectDictionary);
+                        }
+
+                        var body = SqlHelpers.GenerateJsonData(results);
+
+                        return new Response
+                        {
+                            ContentType = ContentTypes.JsonApplication,
+                            ResponseCode = ResponseCode.Ok,
+                            Type = ResponseType.Text,
+                            Body = body
+                        };
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Homework - Class09/SedcServer/ServerPlugins/SqlServer/SqlHelpers.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServerPlugins.SqlServer
+{
+    public class SqlHelpers
+    {
+
+        public static string GenerateJsonData(List<Dictionary<string, string>> data)
+        {
+            var objectStrings = data.Select(item => GenerateJsonData(item));
+
+            var objectString = string.Join(",", objectStrings);
+            var splitedObject = objectString.Split(",");
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in splitedObject)
+            {
+                int separator = item.IndexOf(':');
+                int curlyBrace = item.IndexOf("{");
+                var key = item.Substring(curlyBrace + 2, separator - 2);
+                var value = item.Substring(separator);
+                sb.Append(" ");
+                sb.Append(key);
+                sb.AppendLine(value);
+            }
+            return sb.ToString();
+        }
+
+        public static string GenerateJsonData(Dictionary<string, string> item)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(string.Join(",", item.Select(kvp => $"\"{kvp.Key}\": \"{kvp.Value}\"")));
+
+            return sb.ToString();
+        }
+    }
+}
